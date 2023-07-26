521eb4f199e7c897b43809586ddf86a308b4e129
thimira2 <thimira2@illinois.edu>
2023-07-26 18:02:29 -0500

    made changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let countriesData; // Holds the loaded dataset
 // Load the CSV file and start the visualization
 d3.csv("cleaned_world_pop.csv").then(data => {
   countriesData = data.map(d => ({
-    country: d.["Country/Territory"],
+    country: d["Country/Territory"],
     year: +d.Year,
     population: +d.Population
   }));
@@ -19,93 +19,132 @@ function renderScene(sceneIndex) {
 
   // Determine which scene to render based on the sceneIndex
   if (sceneIndex === 0) {
-    renderScene1();
+    renderIntroScene();
   } else if (sceneIndex === 1) {
-    renderScene2();
-  } else if (sceneIndex === 2) {
-    renderScene3();
+    renderPopulationLineChart();
   }
 }
 
-function renderScene1() {
-    const margin = { top: 20, right: 30, bottom: 30, left: 60 };
-    const width = 500 - margin.left - margin.right;
-    const height = 300 - margin.top - margin.bottom;
+function renderIntroScene() {
+    // Remove existing chart elements
+    d3.select("#chart-container").html("");
   
-    const svg = d3.select("#chart-container")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+    // Create a new div element to contain the introductory message
+    const introDiv = d3.select("#chart-container")
+      .append("div")
+      .attr("id", "intro-div")
+      .style("background-color", "#f0f0f0")
+      .style("padding", "20px")
+      .style("margin-bottom", "20px");
   
-    // Parse the data
-    const parseDate = d3.timeParse("%Y");
-    const data = [
-      { year: "1970", population: 822534450 },
-      { year: "1980", population: 982372466 },
-      { year: "1990", population: 1153704252 },
-      { year: "2000", population: 1264099069 },
-      { year: "2010", population: 1348191368 },
-      { year: "2015", population: 1393715448 },
-      { year: "2020", population: 1424929781 },
-      { year: "2022", population: 1425887337 }
-    ];
+    // Add the introductory message to the div
+    introDiv.append("h2")
+      .text("Exploring Population of Leading Polluters");
   
-    // Format the data
-    const formatDate = d3.timeFormat("%Y");
-    data.forEach(d => {
-      d.year = parseDate(d.year);
-    });
-  
-    // Define the scales
-    const xScale = d3.scaleTime()
-      .domain(d3.extent(data, d => d.year))
-      .range([0, width]);
-  
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.population)])
-      .range([height, 0]);
-  
-    // Define the line
-    const line = d3.line()
-      .x(d => xScale(d.year))
-      .y(d => yScale(d.population));
-  
-    // Add the line path
-    svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 2)
-      .attr("d", line);
-  
-    // Add the X-axis
-    svg.append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xScale));
-  
-    // Add the Y-axis
-    svg.append("g")
-      .call(d3.axisLeft(yScale));
-  
-    // Add X-axis label
-    svg.append("text")
-      .attr("transform", `translate(${width / 2},${height + 30})`)
-      .style("text-anchor", "middle")
-      .text("Year");
-  
-    // Add Y-axis label
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Population");
+    introDiv.append("p")
+      .text(`China, the United States, and India emerge as significant contributors to global pollution across multiple sectors, 
+      reflecting the magnitude of their industrial activities and population sizes. Drawing insights from Garret Hardin's Tragedy of the Commons,
+      it becomes evident that population increase plays a pivotal role in exacerbating pollution levels. As these nations experience rapid population growth,
+      the strain on finite resources intensifies, leading to overexploitation and degradation of the environment.
+      The tragedy of the commons highlights how individual actors, in their pursuit of self-interest, tend to deplete shared resources,
+      ultimately leading to collective harm. In the context of China, the United States, and India,
+      the sheer size of their populations magnifies the pressure on energy production, transportation, agriculture, and manufacturing,
+      thereby intensifying pollution levels. Addressing this global challenge requires collaborative efforts, sustainable policies, and innovative technologies to mitigate the adverse impacts of human activities on the environment and preserve the planet for future generations.
+      Furthermore, a comprehensive dashboard has been developed to provide a deeper look into the population habits of these three countries
+      from 1970 to 2022. This interactive dashboard will allow users to explore the physical population growth within each country,
+      gaining valuable insights into the dynamics of their population changes over time.`);
   }
   
 
+function renderPopulationLineChart() {
+  // Your code for rendering the population line chart goes here
+
+  // Filter the data for the selected country
+  const selectedCountry = document.getElementById("country-select").value;
+  const dataForCountry = countriesData.filter(d => d.country === selectedCountry);
+
+  const margin = { top: 20, right: 30, bottom: 30, left: 60 };
+  const width = 500 - margin.left - margin.right;
+  const height = 300 - margin.top - margin.bottom;
+
+  const svg = d3.select("#chart-container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // Define the scales
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(dataForCountry, d => d.year))
+    .range([0, width]);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataForCountry, d => d.population)])
+    .range([height, 0]);
+
+  // Define the line
+  const line = d3.line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d.population));
+
+  // Add the line path
+  svg.append("path")
+    .datum(dataForCountry)
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 2)
+    .attr("d", line);
+
+  // Add the X-axis
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale));
+
+  // Add the Y-axis
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
+
+  // Add X-axis label
+  svg.append("text")
+    .attr("transform", `translate(${width / 2},${height + 30})`)
+    .style("text-anchor", "middle")
+    .text("Year");
+
+  // Add Y-axis label
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Population");
+}
+
+// Add event listener for the country dropdown
+const countrySelect = document.getElementById("country-select");
+countrySelect.addEventListener("change", function() {
+  // Get the selected country value
+  const selectedCountry = this.value;
+
+  // Call a function to update the page or redirect to the next scene
+  goToNextScene(selectedCountry);
+});
+
+// Function to handle navigation to the next scene based on the selected country
+function goToNextScene(selectedCountry) {
+  // Perform actions based on the selected country
+  // For example, update the chart based on the selected country
+
+  // Assuming you have different renderScene functions for each country
+  if (selectedCountry === "China" || selectedCountry === "India" || selectedCountry === "United States") {
+    currentScene = 1; // Move to the next scene with the population line chart
+    renderScene(currentScene);
+  } else {
+    // Default behavior for other countries or the "All Countries" option
+    // Add code here if needed
+  }
+}
 
 
