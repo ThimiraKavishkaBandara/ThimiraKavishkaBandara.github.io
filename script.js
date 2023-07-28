17165a9c9cb7010e12da2937ab657967c642a2fd
thimira2 <thimira2@illinois.edu>
2023-07-27 22:23:39 -0500

    errors fixed

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let currentScene = 0;
 let countriesData; // Holds the loaded dataset
 
 // Load the CSV file and start the visualization
-d3.csv("cleaned_world_pop.csv").then(data => {
+d3.csv("cleaned_world_pop_2.csv").then(data => {
+  console.log('data ===', data)
   countriesData = data.map(d => ({
     country: d["Country/Territory"],
     year: +d.Year,
     population: +d.Population
   }));
-
   renderScene(currentScene);
 });
 
@@ -26,23 +26,23 @@ function renderScene(sceneIndex) {
 }
 
 function renderIntroScene() {
-    // Remove existing chart elements
-    d3.select("#chart-container").html("");
-  
-    // Create a new div element to contain the introductory message
-    const introDiv = d3.select("#chart-container")
-      .append("div")
-      .attr("id", "intro-div")
-      .style("background-color", "#f0f0f0")
-      .style("padding", "20px")
-      .style("margin-bottom", "20px");
-  
-    // Add the introductory message to the div
-    introDiv.append("h2")
-      .text("Exploring Population of Leading Polluters");
-  
-    introDiv.append("p")
-      .text(`China, the United States, and India emerge as significant contributors to global pollution across multiple sectors, 
+  // Remove existing chart elements
+  d3.select("#chart-container").html("");
+
+  // Create a new div element to contain the introductory message
+  const introDiv = d3.select("#message")
+    .append("div")
+    .attr("id", "intro-div")
+    .style("background-color", "#f0f0f0")
+    .style("padding", "20px")
+    .style("margin-bottom", "20px");
+
+  // Add the introductory message to the div
+  introDiv.append("h2")
+    .text("Exploring Population of Leading Polluters");
+
+  introDiv.append("p")
+    .text(`China, the United States, and India emerge as significant contributors to global pollution across multiple sectors, 
       reflecting the magnitude of their industrial activities and population sizes. Drawing insights from Garret Hardin's Tragedy of the Commons,
       it becomes evident that population increase plays a pivotal role in exacerbating pollution levels. As these nations experience rapid population growth,
       the strain on finite resources intensifies, leading to overexploitation and degradation of the environment.
@@ -53,15 +53,16 @@ function renderIntroScene() {
       Furthermore, a comprehensive dashboard has been developed to provide a deeper look into the population habits of these three countries
       from 1970 to 2022. This interactive dashboard will allow users to explore the physical population growth within each country,
       gaining valuable insights into the dynamics of their population changes over time.`);
-  }
-  
+}
+
 
 function renderPopulationLineChart() {
   // Your code for rendering the population line chart goes here
 
   // Filter the data for the selected country
   const selectedCountry = document.getElementById("country-select").value;
-  const dataForCountry = countriesData.filter(d => d.country === selectedCountry);
+  console.log('selectedCountry ===', selectedCountry)
+  const dataForCountry = selectedCountry ? countriesData.filter(d => d.country === selectedCountry) : countriesData;
 
   const margin = { top: 20, right: 30, bottom: 30, left: 60 };
   const width = 500 - margin.left - margin.right;
@@ -117,19 +118,30 @@ function renderPopulationLineChart() {
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left)
     .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
+    .attr("dy", "5em")
     .style("text-anchor", "middle")
     .text("Population");
 }
 
 // Add event listener for the country dropdown
 const countrySelect = document.getElementById("country-select");
-countrySelect.addEventListener("change", function() {
+countrySelect.addEventListener("change", function () {
   // Get the selected country value
   const selectedCountry = this.value;
-
+  console.log('countrySelect ===', this.value)
   // Call a function to update the page or redirect to the next scene
   goToNextScene(selectedCountry);
+  // add more text
+  document.getElementById('populate_info').innerHTML = '';
+  const populateInfoDiv = d3.select("#populate_info")
+    .append("div")
+    .attr("id", "populate_info_div")
+    .style("background-color", "#f0f0f0")
+    .style("padding", "20px")
+    .style("margin-bottom", "20px");
+
+    populateInfoDiv.append("p")
+    .text(`selected Country  ${this.value}`);
 });
 
 // Function to handle navigation to the next scene based on the selected country
@@ -144,6 +156,7 @@ function goToNextScene(selectedCountry) {
   } else {
     // Default behavior for other countries or the "All Countries" option
     // Add code here if needed
+    renderScene(1);
   }
 }
 
